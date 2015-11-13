b26a6ed34bbb773de3f61212c733ca0ccd407e5e
Kenneth Lewenhagen <lewenhagen@live.com>
2015-11-13 09:56:48 +0100

    small form elements + js

---
+++ example/handleFormElements/main.js
@@ -0,0 +1,30 @@
+/**
+ * Form examples
+ */
+window.onload = function() {
+    "use strict";
+
+    var myForm = document.getElementById("myForm");
+    var chooseColor = document.getElementById("chooseColor");
+
+    var submitButton = document.getElementById("submitButton");
+    var clearButton = document.getElementById("clearButton");
+
+    chooseColor.addEventListener("change", function() {
+        myForm.style.backgroundColor = chooseColor.value;
+    });
+
+    submitButton.addEventListener("click", function() {
+        document.getElementById("firstnameResult").innerHTML = "Name: " + document.getElementById("firstname").value + " ";
+        document.getElementById("lastnameResult").innerHTML = document.getElementById("lastname").value;
+        document.getElementById("passwordResult").innerHTML = "Password: " + document.getElementById("password").value;
+        document.getElementById("numberResult").innerHTML = "Chosen number: " + document.getElementById("number").value;
+        document.getElementById("textareaResult").innerHTML = "Textmessage: " + document.getElementById("textarea").value;
+    });
+
+    clearButton.addEventListener("click", function() {
+        location.reload();
+    });
+
+    console.log("Everything is ready.");
+}();
