a257aa73dd8035b9abf4b6e7784ab966a421a6fc
Mikael Roos <mikael.t.h.roos@gmail.com>
2015-11-13 13:00:59 +0100

    adding sandbox webgl

--- example/canvas-on-full-screen/main.js
+++ example/canvas-on-full-screen/main.js
@@ -4,23 +4,8 @@
 window.onload = function() {
     "use strict";
 
-    var canvas, ct;
-
-    // Get the HTML element to work with
-    canvas = document.getElementById("canvas1");
-
-    // Create a 2D context on the canvas
-    ct = canvas.getContext("2d");
-
-
-
-    /**
-     * Set the size of the canvas.
-     */
-    function resize() {
-        ct.canvas.width  = window.innerWidth;
-        ct.canvas.height = window.innerHeight;
-    } 
+    var canvas = document.getElementById("canvas1"),
+        ctx    = canvas.getContext("2d");
 
 
 
@@ -33,126 +18,73 @@ window.onload = function() {
         // Sizes
         x0 = 1;
         y0 = 1;
-        x1 = ct.canvas.width - 1;
-        y1 = ct.canvas.height - 1;
+        x1 = ctx.canvas.width - 1;
+        y1 = ctx.canvas.height - 1;
         w = 100;
         h = 100;
         
         // top left (almost)
-        ct.fillStyle = "hsla(0,100%,50%,1)";
-        ct.fillRect(x0, y0, w, h);
+        ctx.fillStyle = "hsla(0,100%,50%,1)";
+        ctx.fillRect(x0, y0, w, h);
 
         // top right (almost)
-        ct.fillStyle = "hsla(90,100%,50%,0.8)";
-        ct.fillRect(x1 - w, y0, w, h);
+        ctx.fillStyle = "hsla(90,100%,50%,0.8)";
+        ctx.fillRect(x1 - w, y0, w, h);
 
         // bottom left (almost)
-        ct.fillStyle = "hsla(180,100%,50%,0.6)";
-        ct.fillRect(x0, y1 - h, w, h);
+        ctx.fillStyle = "hsla(180,100%,50%,0.6)";
+        ctx.fillRect(x0, y1 - h, w, h);
 
         // bottom right (almost)
-        ct.fillStyle = "hsla(270,100%,50%,0.4)";
-        ct.fillRect(x1 - w, y1 - h, w, h);
+        ctx.fillStyle = "hsla(270,100%,50%,0.4)";
+        ctx.fillRect(x1 - w, y1 - h, w, h);
     } 
 
 
 
     /**
-     * Check if full screen is enabled.
+     * Event handler for pressed keys.
      */
-    function fullscreenEnabled() {
-        var enabled = document.fullscreenEnabled || 
-                      document.webkitFullscreenEnabled || 
-                      document.mozFullScreenEnabled ||
-                      document.msFullscreenEnabled;
+    function keypress(event) {
+        console.log("Keypress " + event.keyCode);
         
-        console.log("Fullscren is enabled: " + enabled);
-        return enabled;
+        switch (event.keyCode) {
+            case 0:    //ContextMenu
+            case 13:   //Enter
+            case 32:   //Space
+            case 39:   //ArrowRight
+            case 40:   //ArrowDown
+                //play();
+            break;
+            case 8:    //Backspace
+            case 37:   //ArrowLeft
+            case 38:   //ArrowUp
+                //play(true);
+            break;
+            case 70:  //f
+                Canvas.resizeFullBrowserSize();
+                draw();
+            break;
+            case 190:  //Period
+                Canvas.resizeDefault();
+                draw();
+            break;
+            default:
+                return;
+            };
+
+        return false;
     }
 
 
-
-    /**
-     * Go to fullscreen.
-     */
-    function requestFullscreen(elementId) {
-        var el = document.getElementById(elementId),
-            res = null;
-         
-        if (el.requestFullscreen) {
-            res = el.requestFullscreen();
-        } else if (el.webkeltRequestFullscreen) {
-            res = el.webkeltRequestFullscreen();
-        } else if (el.mozRequestFullScreen) {
-            res = el.mozRequestFullScreen();
-        } else if (el.msRequestFullscreen) {
-            res = el.msRequestFullscreen();
-        }
-        
-        console.log("Fullscreen requested: " + res);
-        return res;
-    }
+    // Create eventhandlers for keypress
+    window.onkeydown = keypress;
 
 
 
-    /**
-     * Check if we are fullscreen, return the element or false
-     */
-    function fullscreenElement() {
-        var el = document.fullscreenElement ||
-                 document.webkitFullscreenElement ||
-                 document.mozFullScreenElement ||
-                 document.msFullscreenElement;
-
-        console.log("The fullscreen element is: " + el);
-        return el;
-    }
-
-
-
-    /**
-     * Check if we are fullscreen, return the element or false
-     */
-    function exitFullscreen() {
-        if (document.exitFullscreen) {
-            document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) {
-            document.webkitExitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-            document.mozCancelFullScreen();
-        } else if (document.msExitFullscreen) {
-            document.msExitFullscreen();
-        }
-    }
-
-
-/*
-    document.addEventListener("fullscreenchange", FShandler);
-    document.addEventListener("webkitfullscreenchange", FShandler);
-    document.addEventListener("mozfullscreenchange", FShandler);
-    document.addEventListener("MSFullscreenChange", FShandler);
-*/
-
-
-/*
-    document.addEventListener("fullscreenerror", FSerrorhandler);
-    document.addEventListener("webkitfullscreenerror", FSerrorhandler);
-    document.addEventListener("mozfullscreenerror", FSerrorhandler);
-    document.addEventListener("MSFullscreenError", FSerrorhandler);
-*/
-
-
-    // Init the size of the canvas and draw som elements
-    //resize();
+    // Start the application 
+    Canvas.init(ctx);
     draw();
     
-    // Add eventhandler to listen for resiz events on window
-    window.addEventListener("resize", function (event) {
-        //resize();
-        draw();
-        console.log("Window was resized.");
-    });
-
-    // All done
     console.log("Everything is ready.");  
 }();
---
+++ example/sandboxWebGL/main.js
@@ -0,0 +1,176 @@
+/**
+ * Draw on Canvas
+ */
+window.onload = function() {
+    "use strict";
+
+    var canvas;
+    var gl;
+    var program;
+    var buffer;
+
+
+
+    // Get A WebGL context
+    canvas = document.getElementById("canvas");
+    gl = WebGLUtils.getWebGLContext(canvas);
+    if (!gl) {
+        return;
+    }
+
+
+
+    // Setup GLSL program
+    /*
+    var vertexShader = WebGLUtils.createShaderFromScriptElement(gl, "vertex-shader");
+    var fragmentShader = WebGLUtils.createShaderFromScriptElement(gl, "fragment-shader");
+    program = WebGLUtils.createProgram(gl, [vertexShader, fragmentShader]);
+    gl.useProgram(program);
+    */
+
+    // Setup GLSL program - shorter version
+    program = WebGLUtils.createProgramFromScripts(gl, ["vertex-shader", "fragment-shader"]);
+    gl.useProgram(program);
+
+
+
+    // Look up vertex data
+    var a_Position = gl.getAttribLocation(program, "a_Position");
+    var u_Translation = gl.getUniformLocation(program, "u_Translation");
+
+
+
+    // Lookup fragment data
+    var u_FragColor = gl.getUniformLocation(program, "u_FragColor");
+
+
+
+    // Create a buffer
+    buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.enableVertexAttribArray(a_Position);
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+
+
+    // Set Geometry
+    //var data = setGeometrySquareHalf(gl);
+    //var data = setGeometrySquareFull(gl);
+    var data = setGeometryTriangle(gl);
+
+
+
+    // Update & Draw
+    update();
+    draw();
+
+
+
+    // Update before drawing
+    function update() {
+        // The translation distance for x, y and z direction
+        var translation = new Float32Array([
+            //0.5, 0.5, 0.5, 0.0 
+            Math.random()-0.5, Math.random()-0.5, Math.random()-0.5, 0.0 
+        ]);
+        gl.uniform4fv(u_Translation, translation);
+
+        // Set the color
+        var color = new Float32Array([
+            //0.0, 1.0, 0.0, 1.0 
+            Math.random(), Math.random(), Math.random(), 1.0 
+        ]);
+        gl.uniform4fv(u_FragColor, color);
+    }
+
+
+
+    // Draw it all
+    function draw() {
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(data.mode, 0, data.n);
+    }
+
+
+
+    console.log(gl);
+    console.log("Everything is ready.");
+}();
+
+
+
+/**
+ * A full square.
+ */
+function setGeometrySquareFull(gl) {
+    var data = {
+        n: 6,
+        mode: gl.TRIANGLES
+    };
+
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            -1.0, -1.0,
+             1.0, -1.0,
+            -1.0,  1.0,
+            -1.0,  1.0,
+             1.0, -1.0,
+             1.0,  1.0
+        ]),
+       gl.STATIC_DRAW
+   );
+
+   return data;
+}
+
+
+
+/**
+ * Half a square.
+ */
+function setGeometrySquareHalf(gl) {
+    var data = {
+        n: 6,
+        mode: gl.TRIANGLES
+    };
+
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            -0.5, -0.5,
+             0.5, -0.5,
+            -0.5,  0.5,
+            -0.5,  0.5,
+             0.5, -0.5,
+             0.5,  0.5
+        ]),
+       gl.STATIC_DRAW
+   );
+   
+  return data;
+}
+
+
+
+/**
+ * Triangle.
+ */
+function setGeometryTriangle(gl) {
+    var data = {
+        n: 3,
+        mode: gl.TRIANGLES
+    };
+
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+             0.0,  0.5,
+            -0.5, -0.5,
+             0.5, -0.5,
+        ]),
+       gl.STATIC_DRAW
+   );
+   
+  return data;
+}
