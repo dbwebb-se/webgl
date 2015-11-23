d2d76eae584085f50f72540a3c45eb1f65518adc
Kenneth Lewenhagen <lewenhagen@live.com>
2015-11-23 13:02:43 +0100

    corrected functionality

--- example/HTML5Audio/main.js
+++ example/HTML5Audio/main.js
@@ -1,8 +1,20 @@
 /**
- * Draw on Canvas
+ * Html5 audio
  */
 window.onload = function() {
     "use strict";
 
-    
+    var audio = document.getElementById("music");
+    var button = document.getElementById("toggleBackgroundMusic");
+    button.disabled = false;
+
+    button.addEventListener("click", function() {
+        if (audio.paused) {
+            audio.play();
+            button.innerHTML = "Pausa";
+        } else {
+            audio.pause();
+            button.innerHTML = "Spela";
+        }
+    });
 }
