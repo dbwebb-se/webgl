d7239ead6e255bcb2dddebe0ccaf5be1bea71700
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-10-10 18:05:19 +0200

    Added eslint, customized it and passed validation for example/.

---
+++ example/archive/me/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * JS
+ */
+/* global $ */
+(function () {
+    'use strict';
+
+    // Init jQm widgets
+    $("#header").toolbar();
+    $("#footer").toolbar();
+    $("#menu").panel().enhanceWithin();
+})();
