350e90a504f50f0228d9b2372eb493c49f216229
Emil Folino BTH MAC <emilfolino@gmail.com>
2018-09-04 11:57:19 +0200

    Fixed validation errors for asteroids

--- example/webgl/asteroids/mosAsteroids/main.js
+++ example/webgl/asteroids/mosAsteroids/main.js
@@ -1,42 +1,42 @@
 /**
- * Playing Asteroids while learning JavaScript object model.
- */
+* Playing Asteroids while learning JavaScript object model.
+*/
 
 /**
- * Shim layer, polyfill, for requestAnimationFrame with setTimeout fallback.
- * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
- */
-window.requestAnimFrame = (function(){
+* Shim layer, polyfill, for requestAnimationFrame with setTimeout fallback.
+* http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+*/
+window.requestAnimFrame = (function() {
     return  window.requestAnimationFrame       ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame    ||
-            window.oRequestAnimationFrame      ||
-            window.msRequestAnimationFrame     ||
-            function( callback ){
-                window.setTimeout(callback, 1000 / 60);
-            };
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame    ||
+    window.oRequestAnimationFrame      ||
+    window.msRequestAnimationFrame     ||
+    function( callback ) {
+        window.setTimeout(callback, 1000 / 60);
+    };
 })();
 
 
 
 /**
- * Shim layer, polyfill, for cancelAnimationFrame with setTimeout fallback.
- */
-window.cancelRequestAnimFrame = (function(){
+* Shim layer, polyfill, for cancelAnimationFrame with setTimeout fallback.
+*/
+window.cancelRequestAnimFrame = (function() {
     return  window.cancelRequestAnimationFrame       ||
-            window.webkitCancelRequestAnimationFrame ||
-            window.mozCancelRequestAnimationFrame    ||
-            window.oCancelRequestAnimationFrame      ||
-            window.msCancelRequestAnimationFrame     ||
-            window.clearTimeout;
+    window.webkitCancelRequestAnimationFrame ||
+    window.mozCancelRequestAnimationFrame    ||
+    window.oCancelRequestAnimationFrame      ||
+    window.msCancelRequestAnimationFrame     ||
+    window.clearTimeout;
 })();
 
 
 
 /**
- * True prototypal inheritance by Douglas Crockford.
- * http://javascript.crockford.com/prototypal.html
- */
+* True prototypal inheritance by Douglas Crockford.
+* http://javascript.crockford.com/prototypal.html
+*/
 Object.create = function (o) {
     function F() {}
     F.prototype = o;
@@ -47,9 +47,9 @@ Object.create = function (o) {
 
 
 /**
- * Trace the keys pressed
- * http://nokarma.org/2011/02/27/javascript-game-development-keyboard-input/index.html
- */
+* Trace the keys pressed
+* http://nokarma.org/2011/02/27/javascript-game-development-keyboard-input/index.html
+*/
 window.Key = {
     pressed: {},
 
@@ -75,35 +75,50 @@ window.Key = {
         delete this.pressed[event.keyCode];
     }
 };
-window.addEventListener('keyup',   function(event) { Key.onKeyup(event); },   false);
-window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
+window.addEventListener('keyup',   function(event) {
+    window.Key.onKeyup(event);
+}, false);
+window.addEventListener('keydown', function(event) {
+    window.Key.onKeydown(event);
+}, false);
 
 
 
 /**
- * All objects are Vectors
- */
+* All objects are Vectors
+*/
 function Vector(x, y) {
     this.x = x || 0;
     this.y = y || 0;
 }
 
 Vector.prototype = {
-    muls:  function (scalar) { return new Vector( this.x * scalar, this.y * scalar); }, // Multiply with scalar
-    imuls: function (scalar) { this.x *= scalar; this.y *= scalar; return this; },      // Multiply itself with scalar
-    adds:  function (scalar) { return new Vector( this.x + scalar, this.y + scalar); }, // Add with scalar
-    iadd:  function (vector) { this.x += vector.x; this.y += vector.y; return this; }   // Add itself with Vector
+    muls:  function (scalar) {
+        return new Vector(this.x * scalar, this.y * scalar);
+    }, // Multiply with scalar
+    imuls: function (scalar) {
+        this.x *= scalar;
+        this.y *= scalar;
+        return this;
+    },      // Multiply itself with scalar
+    adds:  function (scalar) {
+        return new Vector(this.x + scalar, this.y + scalar);
+    }, // Add with scalar
+    iadd:  function (vector) {
+        this.x += vector.x;
+        this.y += vector.y;
+        return this;
+    }   // Add itself with Vector
 };
 
 
 
-
 /**
- * Particles as parts of a particle system
- */
+* Particles as parts of a particle system
+*/
 function Particle(position, velocity) {
-    this.position = position || new Vector(0,0);
-    this.velocity = velocity || new Vector(0,0);
+    this.position = position || new Vector(0, 0);
+    this.velocity = velocity || new Vector(0, 0);
 }
 
 Particle.prototype = {
@@ -116,24 +131,24 @@ Particle.prototype = {
 
 
 /**
- * The particle system
- */
-function ParticleSystem(particles, forces) {
-    this.particles  = particles || [];
-    this.forces     = forces    || [];
-}
+* The particle system
+*/
+// function ParticleSystem(particles, forces) {
+//     this.particles  = particles || [];
+//     this.forces     = forces    || [];
+// }
 
 Particle.prototype = {
 
     update: function(td) {
         var alive = [], i, j, particle, force;
-        for(i=0; i<this.particles.length; i++) {
+        for (i=0; i<this.particles.length; i++) {
             particle = this.particles[i];
-            for(j=o; j<this.forces.length; j++) {
+            for (j=0; j<this.forces.length; j++) {
                 force = this.forces[i];
                 force(particle, td);
             }
-            if(particle.update(td)) {
+            if (particle.update(td)) {
                 alive.push(particle);
             }
         }
@@ -142,7 +157,7 @@ Particle.prototype = {
 
     draw: function(ct) {
         var i;
-        for(i=0; i<this.particles.length; i++) {
+        for (i=0; i<this.particles.length; i++) {
             this.particle[i].draw(ct);
         }
     }
@@ -150,8 +165,8 @@ Particle.prototype = {
 
 
 /**
- * The forces around us.
- */
+* The forces around us.
+*/
 function Forces() {
     this.all = {};
 }
@@ -181,7 +196,7 @@ Forces.prototype = {
     addWind:          function(name, vector)  { this.all[name] = this.createWind(vector); },
 
     update: function(object, td) {
-        for(var force in this.all) {
+        for (var force in this.all) {
             if (this.all.hasOwnProperty(force)) {
                 this.all[force](object, td);
             }
@@ -198,9 +213,10 @@ window.Forces.addWind('wind', new Vector(0.5, 0));
 
 
 /**
- * A Player as an object.
- */
-function Player(width, height, position, velocity, speed, direction, accelerateForce, breakForce, dampForce) {
+* A Player as an object.
+*/
+function Player(width, height, position, velocity, speed,
+    direction, accelerateForce, breakForce, dampForce) {
     this.height     = height    || 32;
     this.width      = width     || 32;
     this.position   = position  || new Vector();
@@ -213,88 +229,93 @@ function Player(width, height, position, velocity, speed, direction, accelerateF
 }
 
 Player.prototype = {
-
     draw: function(ct) {
-    var x = this.width/2, y = this.height/2;
+        var x = this.width/2, y = this.height/2;
 
-    ct.save();
-    ct.translate(this.position.x, this.position.y);
-    ct.rotate(this.direction+Math.PI/2);
-    ct.beginPath();
-    ct.moveTo(0, -y);
-    ct.lineTo(x, y);
-    ct.lineTo(0, 0.8*y);
-    ct.lineTo(-x, y);
-    ct.lineTo(0, -y);
+        ct.save();
+        ct.translate(this.position.x, this.position.y);
+        ct.rotate(this.direction+Math.PI/2);
+        ct.beginPath();
+        ct.moveTo(0, -y);
+        ct.lineTo(x, y);
+        ct.lineTo(0, 0.8*y);
+        ct.lineTo(-x, y);
+        ct.lineTo(0, -y);
 
-    if (Key.isDown(Key.UP, Key.W)) {
-        ct.moveTo(0, y);
-        ct.lineTo(-2, y+10);
-        ct.lineTo(0, y+8);
-        ct.lineTo(2, y+10);
-        ct.lineTo(0, y);
+        if (window.Key.isDown(window.Key.UP, window.Key.W)) {
+            ct.moveTo(0, y);
+            ct.lineTo(-2, y+10);
+            ct.lineTo(0, y+8);
+            ct.lineTo(2, y+10);
+            ct.lineTo(0, y);
+        }
+
+        if (window.Key.isDown(window.Key.DOWN, window.Key.S)) {
+            ct.moveTo(y+4, 0);
+            ct.arc(0, 0, y+4, 0, Math.PI, true);
+        }
+
+        ct.stroke();
+        ct.restore();
+    },
+
+    moveForward: function(td) {
+        this.dampForce(this.speed, td);
+        this.position.x += this.speed.x * Math.cos(this.direction) * td;
+        this.position.y += this.speed.y * Math.sin(this.direction) * td;
+        this.position.iadd(this.velocity.muls(td));
+    },
+
+    rotateLeft:  function() { this.direction -= Math.PI/30; },
+    rotateRight: function() { this.direction += Math.PI/30; },
+
+    throttle: function(td)  { this.accelerateForce(this.speed, td); },
+    breaks:   function(td)  {
+        this.breakForce(this.speed, td);
+        this.breakForce(this.velocity, td);
+    },
+
+    update: function(td, width, height) {
+        if (window.Key.isDown(window.Key.UP, window.Key.W)) {
+            this.throttle(td);
+        }
+        if (window.Key.isDown(window.Key.LEFT, window.Key.A)) {
+            this.rotateLeft();
+        }
+        if (window.Key.isDown(window.Key.DOWN, window.Key.S)) {
+            this.breaks(td);
+        }
+        if (window.Key.isDown(window.Key.RIGHT, window.Key.D)) {
+            this.rotateRight();
+        }
+        Forces.update(this.velocity, td);
+        //this.position.iadd(this.velocity.muls(td))
+        this.moveForward(td);
+        this.stayInArea(width, height);
+    },
+
+    stayInArea: function(width, height) {
+        if (this.position.y < -this.height) {
+            this.position.y = height;
+        }
+        if (this.position.y > height) {
+            this.position.y = -this.height;
+        }
+        if (this.position.x > width) {
+            this.position.x = -this.width;
+        }
+        if (this.position.x < -this.width) {
+            this.position.x = width;
+        }
     }
-
-    if (Key.isDown(Key.DOWN, Key.S)) {
-        ct.moveTo(y+4, 0);
-        ct.arc(0, 0, y+4, 0, Math.PI, true);
-    }
-
-    ct.stroke();
-    ct.restore();
-  },
-
-
-  moveForward: function() {
-    this.dampForce(this.speed, td);
-    this.position.x += this.speed.x * Math.cos(this.direction) * td;
-    this.position.y += this.speed.y * Math.sin(this.direction) * td;
-    this.position.iadd(this.velocity.muls(td));
-  },
-
-  rotateLeft:  function() { this.direction -= Math.PI/30; },
-  rotateRight: function() { this.direction += Math.PI/30; },
-
-  throttle: function(td)  { this.accelerateForce(this.speed, td); },
-  breaks:   function(td)  { this.breakForce(this.speed, td); this.breakForce(this.velocity, td); },
-
-/*
-  emitThrottleSparks: function() {
-    var particles = [], x, y;
-    for(i=0; i<10; i++) {
-      x = this.position.x - Math.cos(this.direction)  * Math.random() * 10;
-      y = this.position.y - Math.sin(-this.direction) * Math.random() * 30;
-      position = new Vector(x, y);
-      particles[i] = new Particle(position);
-    }
-  },
-*/
-
-  update: function(td, width, height) {
-    if (Key.isDown(Key.UP, Key.W))     this.throttle(td);
-    if (Key.isDown(Key.LEFT, Key.A))   this.rotateLeft();
-    if (Key.isDown(Key.DOWN, Key.S))   this.breaks(td);
-    if (Key.isDown(Key.RIGHT, Key.D))  this.rotateRight();
-    Forces.update(this.velocity, td);
-    //this.position.iadd(this.velocity.muls(td))
-    this.moveForward(td);
-    this.stayInArea(width, height);
-  },
-
-  stayInArea: function(width, height) {
-    if(this.position.y < -this.height)  this.position.y = height;
-    if(this.position.y > height)        this.position.y = -this.height;
-    if(this.position.x > width)         this.position.x = -this.width;
-    if(this.position.x < -this.width)   this.position.x = width;
-  }
 };
 
 
 
 /**
- * Asteroids, the Game
- */
-window.Asteroids = (function(){
+* Asteroids, the Game
+*/
+window.Asteroids = (function() {
     var ct, ship, lastGameTick, width, height;
 
     var init = function(canvas) {
@@ -303,7 +324,7 @@ window.Asteroids = (function(){
         width = canvas.width;
         height = canvas.height;
         ct.lineWidth = 1;
-        ct.strokeStyle = 'hsla(0,0%,100%,1)';
+        ct.strokeStyle = 'hsla(0, 0%, 100%, 1)';
         ship = new Player(10, 20, new Vector(width/2, height/2));
 
         console.log('Init the game');
@@ -314,15 +335,15 @@ window.Asteroids = (function(){
     };
 
     var render = function() {
-        ct.clearRect(0,0,width,height);
+        ct.clearRect(0, 0, width, height);
         ship.draw(ct);
     };
 
     var gameLoop = function() {
         var now = Date.now();
-        td = (now - (lastGameTick || now)) / 1000; // Timediff since last frame / gametick
+        var td = (now - (lastGameTick || now)) / 1000; // Timediff since last frame / gametick
         lastGameTick = now;
-        requestAnimFrame(gameLoop);
+        window.requestAnimFrame(gameLoop);
         update(td);
         render();
     };
@@ -333,12 +354,11 @@ window.Asteroids = (function(){
     };
 })();
 
-window.onload = function() {
+window.onload = (function() {
     'use strict';
 
-    Asteroids.init('canvas1');
-    Asteroids.gameLoop();
+    window.Asteroids.init('canvas1');
+    window.Asteroids.gameLoop();
 
     console.log('Ready to play.');
-
-}();
+})();
